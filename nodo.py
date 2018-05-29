1711f8e2d42b99e28f66c7f4b35247eadcbfeb3b
Edgardo <edgardo231096@hotmail.com>
2018-05-29 18:53:42 -0500

    copias individuales

--- nodo.py
+++ nodo.py
@@ -3,6 +3,7 @@ from Ciudad import *
 import random
 import math
 import time
+import copy
 file = open("solucion.txt","w")
 gen=1
 
@@ -53,9 +54,10 @@ class Nodo(object):
             i+=1
         print(cad)
 
-    def printOrden(self):
+    def printOrden(self,num):
         aux=self.acomodo
-        aux.append(1)
+        if num==0:
+            aux.append(1)
         return aux
         #file.write(""+str(aux))
         #file.write("\n")
@@ -93,7 +95,7 @@ class Nodo(object):
         return segAcomodo
 
     def cambioAcomodo(self,lista,city): #cambia el orden de las ciudades despues del cruce
-        self.acomodo=lista
+        self.acomodo=copy.deepcopy(lista)
         i=0
         while i<25:
             self.ciudades[i]=(city[(self.acomodo[i])-1])
@@ -119,7 +121,7 @@ while (i<100):
     lista.append(Nodo(nodos))
     lista[i].cromosoma=i+1
     #time.sleep(0.03)
-    file.write("{:<15} {:<15} {:<100} {:<150}".format(str(lista[i].generacion),str(lista[i].cromosoma),str(lista[i].printOrden()),str(lista[i].getDist())))
+    file.write("{:<15} {:<15} {:<100} {:<150}".format(str(lista[i].generacion),str(lista[i].cromosoma),str(lista[i].printOrden(0)),str(lista[i].getDist())))
     file.write("\n")
     #print('distancia:  '+lista[i].getDist())
     i+=1
@@ -128,18 +130,24 @@ lista = sorted(lista, key=lambda nodo: nodo.distancia)
 i=0
 lista2=[]
 nuevaGen=[]
-#print("----------------ordenados")
+print("----------------ordenados")
+file.write("\n")
 while(i<50):
-    #print(str(lista[i].getDist()))
+    file.write("{:<15} {:<15} {:<100} {:<150}".format(str(lista[i].generacion),str(lista[i].cromosoma),str(lista[i].printOrden(1)),str(lista[i].getDist())))
+    file.write("\n")
+    #print(str(lista[i].printOrden())+" "+str(lista[i].getDist()))
     i+=1
 print('--------------------------------')
 i=0
 while (i<50):
     lista2.append(lista[i])
     i+=1
-nuevaGen=lista
-r=0
+#nuevaGen=lista
+#lista2=copy.deepcopy(lista)
+nuevaGen=copy.deepcopy(lista)
 
+r=0
+cont=0
 #print('--------------------------------')
 #print(lista2[0].getAcomodo())
 #print(lista2[0].getDist())
@@ -152,29 +160,32 @@ r=0
 #print(aux.getDist())
 #print('--------------------------------')
 
-#for i in range(50):
-    #for k in range(2):
-        #if i==48:
-            #if k!=1:
-                #nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[49].getAcomodo()),nodos)
-            #else:
-                #nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[0].getAcomodo()),nodos)
-        #elif i==49:
-            #if k!=1:
-                #nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[0].getAcomodo()),nodos)
-            #else:
-                #nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[1].getAcomodo()),nodos)
-        #else:
-            #nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[(i+k+1)].getAcomodo()),nodos)
-        #r+=1
-for i in range(47):
+for i in range(50):
     for k in range(2):
-        nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[(i+k+1)].getAcomodo()),nodos)
+        if i==48:
+            if k!=1:
+                nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[49].getAcomodo()),nodos)
+            else:
+                nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[0].getAcomodo()),nodos)
+        elif i==49:
+            if k!=1:
+                nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[0].getAcomodo()),nodos)
+            else:
+                nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[1].getAcomodo()),nodos)
+        else:
+            nuevaGen[r].cambioAcomodo(lista2[i].cruce(lista2[(i+k+1)].getAcomodo()),nodos)
         r+=1
+
+
 nuevaGen = sorted(nuevaGen, key=lambda nodo: nodo.distancia)
 i=0
 while (i<100):
-    print(str(nuevaGen[i].getDist()))
+    print(str(i)+" "+str(nuevaGen[i].printOrden(1))+" "+str(nuevaGen[i].getDist()))
+    #nuevaGen[i].cambioAcomodo(lista2[uno[i]].cruce(lista2[dos[i]].getAcomodo()),nodos)
+    #print(str(i)+" orden: "+str(nuevaGen[i].printOrden(1))+" --distancia-- "+str(nuevaGen[i].getDist()))
     i+=1
 
+
+#print(uno)
+#print(dos)
 file.close();
